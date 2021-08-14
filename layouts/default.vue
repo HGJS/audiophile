38cc9d65597a6870683b9506eb070d04f063478a
Sam Hellewell <samhellewell@gmail.com>
2021-08-14 17:56:57 +0100

    Create header and footer components Create default layout

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <the-header />
+    <transition>
+      <nuxt />
+    </transition>
+    <the-footer />
+  </div>
+</template>
+
+<script>
+import TheHeader from '@/components/TheHeader.vue'
+import TheFooter from '@/components/TheFooter.vue'
+
+export default {
+  name: 'Default',
+  components: {
+    'the-header': TheHeader,
+    'the-footer': TheFooter,
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div></div>
+  <main>Home page</main>
 </template>
 
 <script>
